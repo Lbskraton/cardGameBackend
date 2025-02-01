ccbd24903a5a948c0e859b2f9c8cf7fe2b7f5bd9
Luis Blanco <lbskyraton@gmail.com>
2025-02-01 20:30:46 +0100

    turso update 6

--- src/auth/router/auth.router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var auth_service_1 = require("../service/auth.service");
-var auth_middleware_1 = require("../../middleware/auth.middleware");
-var router = (0, express_1.Router)();
-router.post('/login', auth_middleware_1.isAuthenticate, auth_service_1.default.login);
-router.post('/register', auth_middleware_1.isAuthenticate, auth_service_1.default.register);
-exports.default = router;
--- src/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var app_1 = require("./app");
-var port = process.env.PORT || 3000;
-app_1.default.listen(port, function () {
-    console.log("servidor encendido en el puerto " + port);
-});
