91594fc32fc50eb652b44a3e14ba07d73bfb7382
Lbskraton <lbskyraton@gmail.com>
2025-02-13 10:08:44 +0100

    Cors correction

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,8 @@ app.use(express.json())
 app.use(cors({
     origin: ['*','https://cardgamefrontend.onrender.com'],
     methods:['GET','POST','PUT','DELETE'],
-    credentials:true
+    credentials:true,
+    allowedHeaders: ['Content-Type', 'Authorization']
 }))
 
 app.use(helmet())
