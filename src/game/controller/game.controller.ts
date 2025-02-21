48e64d10eb00c4820829d68a705888c4b2d414be
Lbskraton <lbskyraton@gmail.com>
2025-02-21 12:21:56 +0100

    GameController

---
+++ src/game/controller/game.controller.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express"
+
+
+//Se comunica con el cliente
+class GameController{
+
+    
+    
+    
+        
+    
+
+  
+    
+}
+
+export default GameController
